1bb7a806ffdf6f85129dcce9098b92c187e71e86
dthiagarajan <dt372@cornell.edu>
2016-08-15 14:59:22 -0400

    finished SVM week 7

---
+++ machine-learning-ex6/README.md
@@ -0,0 +1,3 @@
+# SVM and Kernels
+
+This week's lectures went over the concept and implementation of SVM's and kernels in practice. See the PDF for the modified files
---
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -0,0 +1,53 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+
+
+C_values = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_values = [0.01 0.03 0.1 0.3 1 3 10 30];
+pred_error = 1000;
+
+for Ci=1:length(C_values),
+    local_C = C_values(Ci);
+    for sigmai=1:length(sigma_values),
+        local_sigma = sigma_values(sigmai);
+        model = svmTrain(X, y, local_C, @(x1, x2) gaussianKernel(x1, x2, local_sigma));
+        predictions = svmPredict(model, Xval);
+        local_pred_error = mean(double(predictions ~= yval));
+        if local_pred_error < pred_error,
+            pred_error = local_pred_error;
+            C = local_C;
+            sigma = local_sigma;
+        end
+    end
+end
+
+
+
+
+
+% =========================================================================
+
+end
